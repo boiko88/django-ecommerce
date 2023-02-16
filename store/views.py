c12c327c6e531a43d2c71ca061088385f60f782f
Anton Boiko <bo1ko.ant@yandex.com>
2023-02-16 19:48:24 +0300

    added products to store page with a real price and name

--- store/views.py
+++ store/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
+from .models import *
 
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'store/store.html', context)
 
 
 def cart(request):
     context = {}
+    
     return render(request, 'store/cart.html', context)
 
 
