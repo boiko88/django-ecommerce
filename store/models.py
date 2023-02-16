203fd8e512e5f86f29b77f0da184ff96ab362d82
Anton Boiko <bo1ko.ant@yandex.com>
2023-02-16 15:56:35 +0300

    started working on models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, blank=False, null=True)
+    #image
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered =models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, blank=False, null=True)
+    transaction_id = models.CharField(max_length=200, null=True)
+    
+    
+    def __str__(self):
+        return str(self.id)
+    
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_ordered =models.DateTimeField(auto_now_add=True)
