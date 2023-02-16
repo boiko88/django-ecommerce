ac82fed6c59a03c86b2afde79e6432e90d18b7e1
Anton Boiko <bo1ko.ant@yandex.com>
2023-02-16 19:18:37 +0300

    finished models, created admin panel and made migrations

--- store/models.py
+++ store/models.py
@@ -10,7 +10,7 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
     
-class Product(models.model):
+class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, blank=False, null=True)
@@ -35,4 +35,19 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
-    date_ordered =models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.address
+    
