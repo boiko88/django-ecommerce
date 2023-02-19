bf6540e8db5d53793b8a3e0cd2dd5dd7185de880
Anton Boiko <bo1ko.ant@yandex.com>
2023-02-19 19:17:21 +0300

    made all the data about products dynamic on checkout page

--- store/views.py
+++ store/views.py
@@ -16,10 +16,22 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
+        # Here we have to assign get_cart_total and get_cart_items to zero, otherwise 
+        # if a user is not legged in we will have an error
+        order = {'get_cart_total':0, 'get_cart_items': 0}
     context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        # Here we have to assign get_cart_total and get_cart_items to zero, otherwise 
+        # if a user is not legged in we will have an error
+        order = {'get_cart_total':0, 'get_cart_items': 0}
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
